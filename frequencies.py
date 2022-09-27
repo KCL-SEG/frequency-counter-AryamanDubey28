42be4f31bab4b9e93a25b0629a1aa2e43b1fa27b
Aryaman Dubey <89002729+AryamanDubey28@users.noreply.github.com>
2022-09-27 11:20:19 +0100

    answer

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,25 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    
+    set_items = set(items)
+    
+    for i in set_items:
+        frequencies[str(i)] = getFrequencyOf(i, items)
+
+    
     return frequencies
+
+
+
+
+def getFrequencyOf(letter, list):
+    counter = 0
+    letter = str(letter)
+    for x in list:
+        if str(x) == letter:
+            counter+=1
+    return counter
+
+
+
